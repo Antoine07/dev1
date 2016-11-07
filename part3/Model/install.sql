03acb3a87f0c8d931053be1d77483a59081f0608
Antoine07 <antoine.lucsko@wanadoo.fr>
2016-11-07 15:41:22 +0100

    sql alter table constraint

--- part3/Model/install.sql
+++ part3/Model/install.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS db_moster;
+DROP DATABASE IF EXISTS db_monster;
 
-CREATE DATABASE `db_moster`;
+CREATE DATABASE `db_monster`;
 
-use `db_moster`;
+use `db_monster`;
 
 CREATE TABLE `scores`(
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -10,7 +10,7 @@ CREATE TABLE `scores`(
 	PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+/*
 CREATE TABLE `kermits`(
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`score_id` INT(10) unsigned NULL,
@@ -18,3 +18,16 @@ CREATE TABLE `kermits`(
 	PRIMARY KEY (`id`),
 	CONSTRAINT `kermits_score_id_scores_id` FOREIGN KEY (`score_id`) REFERENCES `scores`(`id`) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+*/
+
+-- on peut également créer les contraintes par la suite pour ne pas avoir de problème de contrainte sur les clés
+
+CREATE TABLE `kermits`(
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`score_id` INT(10) unsigned NULL,
+	`name` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE kermits ADD CONSTRAINT `kermits_score_id_scores_id` FOREIGN KEY (`score_id`) REFERENCES `scores`(`id`) ON DELETE CASCADE;
